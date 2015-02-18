499b54500538c72a1432b2f55afe2af8871d4d84
Allan Harper <aluhadora@gmail.com>
2015-02-17 20:06:49 -0700

    First commit

---
+++ Dark Lyrics Launcher/MainWindow.xaml.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media.Imaging;
+
+namespace Dark_Lyrics_Launcher
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+
+      Icon = Imaging.CreateBitmapSourceFromHBitmap(Properties.Resources.DarklyricsFavicon1.GetHbitmap(), IntPtr.Zero,
+        Int32Rect.Empty,
+        BitmapSizeOptions.FromEmptyOptions());
+    }
+
+    private string Artist { get { return ArtistComboBox.Text; } }
+    private string Song { get { return SongComboBox.Text; } }
+
+    private void ArtistComboBox_KeyDown(object sender, KeyEventArgs e)
+    {
+      if (Artist == string.Empty)
+      {
+        UrlLabel.Content = string.Empty;
+        SongComboBox.IsEnabled = false;
+        return;
+      }
+      if (e.Key == Key.Enter)
+      {
+        if (SongComboBox.IsEnabled)
+        {
+          SongComboBox.Focus();
+        }
+      }
+    }
+
+    private void ArtistComboBox_TextInput(object sender, TextCompositionEventArgs e)
+    {
+
+    }
+
+    private string _lastArtist;
+    private void ArtistComboBox_KeyUp(object sender, KeyEventArgs e)
+    {
+      if (_lastArtist == Artist) return;
+      _lastArtist = Artist;
+
+      if (Artist == string.Empty)
+      {
+        UrlLabel.Content = string.Empty;
+        SongComboBox.IsEnabled = false;
+        return;
+      }
+      if (Song == string.Empty)
+      {
+        UrlLabel.Content = GenerateArtistUrl();
+        if (ArtistComboBox.Items.Cast<ListBoxItem>().Any(x => (string)x.Content == Artist))
+        {
+          StartSongBoxFill();
+        }
+      }
+    }
+
+    private string _lastSong;
+    private void SongComboBox_KeyUp(object sender, KeyEventArgs e)
+    {
+      if (_lastSong == Song) return;
+      _lastSong = Song;
+
+      if (Song == string.Empty)
+      {
+        UrlLabel.Content = GenerateArtistUrl();
+        return;
+      }
+
+      var songs = SongComboBox.Items.Cast<Tuple<string, string>>();
+
+      var songName = Song.Trim();
+      var song = songs.FirstOrDefault(x => x.Item2 == songName);
+      if (song == null)
+      {
+        UrlLabel.Content = string.Empty;
+        return;
+      }
+
+      UrlLabel.Content = song.Item1;
+    }
+
+    private string GenerateArtistUrl()
+    {
+      return WebUtil.GenerateArtistUrl(Artist);
+    }
+
+    private void SongComboBox_GotFocus(object sender, RoutedEventArgs e)
+    {
+      StartSongBoxFill();
+    }
+
+    private void StartSongBoxFill()
+    {
+      SongComboBox.DisplayMemberPath = "Item2";
+      var songs = WebUtil.ArtistSongs(GenerateArtistUrl());
+      
+      SongComboBox.ItemsSource = songs;
+
+      SongComboBox.IsEnabled = true;
+    }
+
+    private void SongComboBox_KeyDown(object sender, KeyEventArgs e)
+    {
+      if (e.Key != Key.Enter) return;
+      if ((string)UrlLabel.Content == string.Empty) return;
+
+      Process.Start(UrlLabel.Content.ToString());
+      Close();
+    }
+
+
+  }
+}
---
+++ Dark Lyrics Launcher/WebUtil.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using HtmlAgilityPack;
+
+namespace Dark_Lyrics_Launcher
+{
+  public class WebUtil
+  {
+    private static WebUtil _instance;
+    private readonly WebClient _client;
+
+    private WebUtil()
+    {
+      _client = new WebClient
+      {
+        Proxy = null,
+      };
+    }
+
+    private static WebUtil Instance()
+    {
+      return _instance ?? (_instance = new WebUtil());
+    }
+
+    public static string GenerateArtistUrl(string artist)
+    {
+      var artistString = artist.Trim().Replace(" ", string.Empty).ToLower();
+      return string.Format("http://www.darklyrics.com/{0}/{1}.html", artistString.Substring(0, 1), artistString);
+    }
+
+    public static IList<Tuple<string, string>> ArtistSongs(string url)
+    {
+      return Instance().ArtistSongsFromInstance(url);
+    }
+
+    private IList<Tuple<string, string>> ArtistSongsFromInstance(string url)
+    {
+      var doc = new HtmlDocument();
+      doc.LoadHtml(_client.DownloadString(new Uri(url)));
+
+      var songs = new List<Tuple<string, string>>();
+
+      foreach (var album in doc.DocumentNode.Descendants("div").Where(d => d.Attributes.Contains("class") && d.Attributes["class"].Value.Contains("album")))
+      {
+        foreach (var songLink in album.Descendants("a"))
+        {
+          var text = songLink.InnerText;
+          var link = "www.darklyrics.com";
+          link += songLink.Attributes["href"].Value.Trim('.');
+
+          var item = Tuple.Create(link, text);
+
+          songs.Add(item);
+        }
+      }
+
+      return songs;
+    }
+  }
+}
